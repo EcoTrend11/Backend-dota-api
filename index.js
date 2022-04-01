9faa03c4b501eb0e8d2bcb0ead8fcea0fe2f0970
EcoTrend11 <bernardo_hl@outlook.com>
2022-04-01 17:21:07 -0500

    make it better

--- index.js
+++ index.js
@@ -9,41 +9,41 @@ app.use('/api', apiRouter)
 
 
 
-sequelize.sync({
-    force: true
-})
+sequelize.sync()
 .then(() => {
-    app.listen(3001, async () => {
+    app.listen(3001, () => {
 
-        let apiAxios = await axios.get('https://api.opendota.com/api/heroStats');
-        let dataApiAxios = apiAxios.data
+        // -----------------ONLY FOR PRECHARGE--------------------
+
+        // let apiAxios = await axios.get('https://api.opendota.com/api/heroStats');
+        // let dataApiAxios = apiAxios.data
         
-        dataApiAxios.forEach(async (e) =>{
+        // dataApiAxios.forEach(async (e) =>{
                        
-            try{
-                const createHeroes = await Heroes.create({
-                    id: e.id,
-                    name : e.localized_name,
-                    primary_atr : e.primary_attr,
-                    attack_type : e.attack_type.toUpperCase(),
-                    role : e.roles,
-                    img : "https://api.opendota.com"+e.img,
-                    base_str : e.base_str,
-                    base_agi : e.base_agi,
-                    base_int : e.base_int,
-                    str_gain : e.str_gain,
-                    agi_gain : e.agi_gain,
-                    int_gain : e.int_gain,
-                    attack_range : e.attack_range,
-                    move_speed : e.move_speed,
-                    attack_rate : e.attack_rate
-                })
+        //     try{
+        //         const createHeroes = await Heroes.create({
+        //             id: e.id,
+        //             name : e.localized_name,
+        //             primary_atr : e.primary_attr,
+        //             attack_type : e.attack_type.toUpperCase(),
+        //             role : e.roles,
+        //             img : "https://api.opendota.com"+e.img,
+        //             base_str : e.base_str,
+        //             base_agi : e.base_agi,
+        //             base_int : e.base_int,
+        //             str_gain : e.str_gain,
+        //             agi_gain : e.agi_gain,
+        //             int_gain : e.int_gain,
+        //             attack_range : e.attack_range,
+        //             move_speed : e.move_speed,
+        //             attack_rate : e.attack_rate
+        //         })
     
-            }
-            catch(error){
-                console.log(error)
-            }
-        })
+        //     }
+        //     catch(error){
+        //         console.log(error)
+        //     }
+        // })
         console.log('app escuchando en el puerto 3001')
     })
 })
--- src/db.js
+++ src/db.js
@@ -1,15 +1,20 @@
 require('dotenv').config()
 const {
-    DB_USER, DB_PASSWORD, DB_HOST,
+    DATABASE_URL
   } = process.env;
 const { Sequelize } = require('sequelize');
 const createHeroesModel = require('../models/heroes')
 
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dota`, {
-    logging: false,
+const sequelize = new Sequelize(`${DATABASE_URL}`, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false
 });
 
-const Heroes = createHeroesModel(sequelize)
 
+const Heroes = createHeroesModel(sequelize)
 module.exports = {sequelize,Heroes}
