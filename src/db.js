3bf8dfcca718df6429d74c408e02b65ec09f02a1
EcoTrend11 <bernardo_hl@outlook.com>
2022-04-04 13:22:57 -0500

    make it better

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const {
 const { Sequelize } = require('sequelize');
 const createHeroesModel = require('../models/heroes')
 
-const sequelize = new Sequelize(`${DATABASE_URL}`, {
+const sequelize = new Sequelize(`postgres://mcrkxgqryqqsbr:f5f64a93f32cd5fce673a6f24f074062d428a656d8764356e3bba4bd31e918cb@ec2-3-209-61-239.compute-1.amazonaws.com:5432/d1hc90oig3c4kd`, {
   dialectOptions: {
     ssl: {
       require: true,
